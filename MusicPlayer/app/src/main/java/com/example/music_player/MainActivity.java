3d44e27a1ef377de6906f748d9b5be8d36d0f7c8
sieun-Bae <qotldms130@gmail.com>
2021-06-02 00:27:17 +0900

    media player toggle

--- MusicPlayer/app/src/main/java/com/example/music_player/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/music_player/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.music_player;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mp = MediaPlayer.create(this, R.raw.roses);
+        mp.setLooping(false);
+
+        tv = (TextView) this.findViewById(R.id.hellobutton);
+        tv.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(!mp.isPlaying()) {
+                    mp.start();
+                    tv.setText("Stop");
+                } else {
+                    mp.pause();
+                    tv.setText("Start");
+                }
+            }
+        });
     }
 }
