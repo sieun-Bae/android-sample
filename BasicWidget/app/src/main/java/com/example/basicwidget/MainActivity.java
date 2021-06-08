3f219691209069627557d8948ca092465604cb77
sieun-Bae <qotldms130@gmail.com>
2021-06-09 03:49:02 +0900

    basic widgets

--- BasicWidget/app/src/main/java/com/example/basicwidget/MainActivity.java
+++ BasicWidget/app/src/main/java/com/example/basicwidget/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.basicwidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //radio button 에서 뭘 선택했는지 알고싶다.
+        RadioButton radioButton = (RadioButton) findViewById(R.id.radioButton);
+        boolean checked = radioButton.isChecked();
+
     }
 }
