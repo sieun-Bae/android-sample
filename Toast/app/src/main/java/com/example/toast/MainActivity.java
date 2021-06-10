88361451865037e0abf7ca47b3e8fa32828d3e14
sieun-Bae <qotldms130@gmail.com>
2021-06-10 20:06:49 +0900

    toast setGravity 수정 필요

--- Toast/app/src/main/java/com/example/toast/MainActivity.java
+++ Toast/app/src/main/java/com/example/toast/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "위치가 바뀐 토스트", Toast.LENGTH_LONG); //토스트 객체 반환
+                toast.setGravity(Gravity.CENTER, 0,0); //위치 지정
+                toast.show(); //토스트 띄우기
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
