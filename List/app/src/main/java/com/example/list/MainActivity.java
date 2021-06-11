9a0ac5bdc7325cc7a5468989c9c67c6af3ccd243
sieun-Bae <qotldms130@gmail.com>
2021-06-12 02:45:23 +0900

    select widget - listview

--- List/app/src/main/java/com/example/list/MainActivity.java
+++ List/app/src/main/java/com/example/list/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -14,11 +16,17 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     SingerAdapter adapter;
+    EditText editText;
+    EditText editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        editText2 = (EditText) findViewById(R.id.editTextTextPersonName2);
 
         ListView listView = (ListView) findViewById(R.id.listView);
 
@@ -35,6 +43,18 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 SingerItem item = (SingerItem) adapter.getItem(position);
                 Toast.makeText(getApplicationContext(), "선택: "+item.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                String mobile = editText2.getText().toString();
+
+                adapter.addItem(new SingerItem(name, mobile, R.drawable.ic_launcher_background));
+                adapter.notifyDataSetChanged();
             }
         });
     }
@@ -64,7 +84,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            SingerItemView view = new SingerItemView(getApplicationContext());
+            SingerItemView view = null;
+            if (convertView == null) {
+                view = new SingerItemView(getApplicationContext());
+            } else {
+                view = (SingerItemView) convertView;
+            }
 
             //position?
             SingerItem item = items.get(position);
