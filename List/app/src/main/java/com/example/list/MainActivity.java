c3d3ea1b4bf7cd93d4934cce92c9d613e972f9b8
sieun-Bae <qotldms130@gmail.com>
2021-06-12 02:02:42 +0900

    why toast not working?

---
+++ List/app/src/main/java/com/example/list/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    SingerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        adapter = new SingerAdapter();
+        adapter.addItem(new SingerItem("엄정화", "010-1000-1000", R.drawable.image1));
+        adapter.addItem(new SingerItem("이효리", "010-2000-2000",R.drawable.image2));
+        adapter.addItem(new SingerItem("제시", "010-3000-3000",R.drawable.image3));
+        adapter.addItem(new SingerItem("화사", "010-4000-4000",R.drawable.image4));
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SingerItem item = (SingerItem) adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "선택: "+item.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    class SingerAdapter extends BaseAdapter { //customizing adapter to make form of item, to make list
+
+        ArrayList<SingerItem> items = new ArrayList<SingerItem>();
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        public void addItem(SingerItem item) {
+            items.add(item);
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            SingerItemView view = new SingerItemView(getApplicationContext());
+
+            //position?
+            SingerItem item = items.get(position);
+            view.setName(item.getName());
+            view.setMobile(item.getMobile());
+            view.setImage(item.getResId());
+            return view;
+        }
+    }
+}
+
+
