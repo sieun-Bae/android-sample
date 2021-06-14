5a5ab8313b3e69878dab68d6db947c6468d7b822
sieun-Bae <qotldms130@gmail.com>
2021-06-14 19:09:10 +0900

    frame layout

--- FrameLayout/app/src/main/java/com/example/framelayout/MainActivity.java
+++ FrameLayout/app/src/main/java/com/example/framelayout/MainActivity.java
@@ -8,35 +8,34 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;
-    ImageView imageView2; //메모리에 할당하여 다른 메소드에서 접근 가능하도록 한다.
 
-    int index = 0;
+    ImageView imageView;
+    ImageView imageView2;
+    int idx = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView)findViewById(R.id.imageView); //return view
-        imageView2 = (ImageView)findViewById(R.id.imageView2);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
     }
 
     public void onButtonClicked(View v) {
+        idx += 1;
 
-    }
-    public void onButton1Clicked(View v) {
-        index += 1;
-        if (index > 1) {
-            index = 0;
+        if (idx > 1) {
+            idx = 0;
         }
 
-        if (index == 0) {
+        if (idx == 0) {
             imageView.setVisibility(View.VISIBLE);
             imageView2.setVisibility(View.INVISIBLE);
-        } else if (index == 1) {
+        } else if (idx == 1) {
             imageView.setVisibility(View.INVISIBLE);
             imageView2.setVisibility(View.VISIBLE);
         }
     }
+
 }
