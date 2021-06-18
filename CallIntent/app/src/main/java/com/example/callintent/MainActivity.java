2a6176b8243ae885480abddce555438f4cf7f780
sieun-Bae <qotldms130@gmail.com>
2021-06-18 11:59:19 +0900

    call intent

--- CallIntent/app/src/main/java/com/example/callintent/MainActivity.java
+++ CallIntent/app/src/main/java/com/example/callintent/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.callintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String receiver = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + receiver));
+                startActivity(intent);
+
+
+                Intent intent2 = new Intent();
+                //2. 객체가 아닌 문자열로 액티비티를 지정하는 방법
+                ComponentName name = new ComponentName("org.example.callintent", "org.example.callintent.MenuActivity");
+                intent2.setComponent(name);
+                startActivity(intent2);
+            }
+        });
     }
 }
