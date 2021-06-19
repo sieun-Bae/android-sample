9a89abab64a357ea9e996c7769e8fbceb0aa64a3
sieun-Bae <qotldms130@gmail.com>
2021-06-20 02:09:28 +0900

    use service to communicate

--- Service/app/src/main/java/com/example/service/MainActivity.java
+++ Service/app/src/main/java/com/example/service/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                //서비스 실행
+                Intent intent = new Intent(getApplicationContext(), MyService.class);
+                intent.putExtra("command", "show");
+                intent.putExtra("name", name);
+                startService(intent);
+            }
+        });
+
+        Intent passedIntent = getIntent();
+        processCommand(passedIntent);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        processCommand(intent);
+    }
+
+    private void processCommand(Intent intent) {
+        if (intent != null) {
+            String command = intent.getStringExtra("command");
+            String name = intent.getStringExtra("name");
+
+            Toast.makeText(this, "서비스로부터 전달받은 데이터:"+command+", "+name, Toast.LENGTH_LONG).show();
+        }
     }
 }
