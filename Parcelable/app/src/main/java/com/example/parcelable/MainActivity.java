18bf06f9e9160d7782483ce40219e57e5fe5c450
sieun-Bae <qotldms130@gmail.com>
2021-06-19 23:35:37 +0900

    menu activity

---
+++ Parcelable/app/src/main/java/com/example/parcelable/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.parcelable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+
+                ArrayList<String> names = new ArrayList<String>();
+                names.add("배시은");
+                names.add("김진수");
+
+                intent.putExtra("names", names);
+                startActivityForResult(intent, 101);
+            }
+        });
+    }
+}
