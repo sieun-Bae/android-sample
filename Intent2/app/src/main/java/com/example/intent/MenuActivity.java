6e1d9df11619b9fc01bc1202c8aea846a3375239
sieun-Bae <qotldms130@gmail.com>
2021-06-21 14:08:46 +0900

    feat: intent project in lec

--- Intent2/app/src/main/java/com/example/intent/MenuActivity.java
+++ Intent2/app/src/main/java/com/example/intent/MenuActivity.java
@@ -2,7 +2,11 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("name", "mike");
+
+                setResult(Activity.RESULT_OK, intent);
+
+                finish();
+            }
+        });
     }
 }
