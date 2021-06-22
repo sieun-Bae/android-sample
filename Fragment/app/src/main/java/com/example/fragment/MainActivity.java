c88b061eb4ad67c59eb881f2221c792ab1f5560c
sieun-Bae <qotldms130@gmail.com>
2021-06-23 01:29:23 +0900

    feat: two fragments

--- Fragment/app/src/main/java/com/example/fragment/MainActivity.java
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -3,22 +3,37 @@ package com.example.fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainFragment fragment1;
+    MenuFragment fragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //fragment 미리 생성
+        fragment1 = new MainFragment();
+        fragment2 = new MenuFragment();
+
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainFragment fragment1 = new MainFragment();
-                getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
             }
         });
     }
---
+++ Fragment/app/src/main/java/com/example/fragment/MenuFragment.java
@@ -0,0 +1,21 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MenuFragment extends Fragment {
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
+
+        return rootView;
+    }
+}
