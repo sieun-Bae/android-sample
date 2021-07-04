7decb485cbdd5cda6ea139825040452b6f10e7f3
sieun-Bae <qotldms130@gmail.com>
2021-07-04 21:54:50 +0900

    feat: camera capture using surfaceView

---
+++ Capture/app/src/main/java/com/example/capture/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.capture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    CameraSurfaceView surfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        surfaceView = (CameraSurfaceView) findViewById(R.id.surfaceView);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capture();
+            }
+        });
+    }
+    public void capture() {
+        surfaceView.capture(new Camera.PictureCallback(){
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 8;
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+                imageView.setImageBitmap(bitmap);
+                camera.startPreview(); //카메라를 찍으면 미리보기가 중단되므로 다시 실행해줘야합니다.
+            }
+        });
+    }
+}
