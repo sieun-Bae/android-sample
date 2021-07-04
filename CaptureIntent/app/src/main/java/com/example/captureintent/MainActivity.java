f278b36a330faed9e9d76a3c3a7b7657d5bda374
sieun-Bae <qotldms130@gmail.com>
2021-07-04 21:27:19 +0900

    fix: uri exception

--- CaptureIntent/app/src/main/java/com/example/captureintent/MainActivity.java
+++ CaptureIntent/app/src/main/java/com/example/captureintent/MainActivity.java
@@ -2,33 +2,42 @@ package com.example.captureintent;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     File file;
+    Uri secureUri;
+    SurfaceView surfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        File sdcard = Environment.getExternalStorageDirectory();
+        File sdcard = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         file = new File(sdcard, "capture.jpg");
+        secureUri = FileProvider.getUriForFile(this, getPackageName(), file);
 
         imageView = (ImageView) findViewById(R.id.imageView);
         Button button = (Button) findViewById(R.id.button);
@@ -41,10 +50,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void capture() {
-
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, secureUri);
+        intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
         startActivityForResult(intent, 101);
+    }
+
+    public void capture_surface() {
+        surfaceView.capture(new Camera.PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                
+            }
+        });
+
+        }
     }
 
     @Override
@@ -53,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 101 && resultCode == Activity.RESULT_OK) {
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inSampleSize = 8;
+
             Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
             imageView.setImageBitmap(bitmap);
         }
